0f9ace9d8d4983df449ff8fe99ba0d4022314908
Guizâo <guilhermesimoes449@gmail.com>
2026-03-08 00:15:15 +0000

    VAI GABRIEL

---
+++ Assets/PieceCleanUp.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class PieceCleanUp : MonoBehaviour
+{
+    private SpriteRenderer pieceSpriteRenderer;
+    private PolygonCollider2D piecePollygonCollider;
+
+    private void Start()
+    {
+        pieceSpriteRenderer = GetComponent<SpriteRenderer>();
+        piecePollygonCollider = GetComponent<PolygonCollider2D>();
+
+        if (IsCompletelyTransparent())
+        {
+            Destroy(gameObject);   
+        }
+
+        FixBrokenColliders();
+    }
+    private void FixBrokenColliders()
+    {
+            if (piecePollygonCollider.points.Length == 5)
+            {
+                Destroy(piecePollygonCollider);
+
+                BoxCollider2D colliderSwitch = gameObject.AddComponent<BoxCollider2D>();
+                colliderSwitch.size = new Vector2(0.1f, 0.1f);
+            }
+    }
+    private bool IsCompletelyTransparent()
+    {
+        Color32[] pixels = pieceSpriteRenderer.sprite.texture.GetPixels32();
+
+        foreach (Color32 pixel in pixels)
+        { 
+            if (pixel.a > 5)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
